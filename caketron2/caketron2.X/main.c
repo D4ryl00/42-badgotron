90a54d354f90af435a607c9ed964048ce3ecb647
elec <elec@elec.com>
2018-03-26 12:01:15 +0200

    try

--- caketron2/caketron2.X/main.c
+++ caketron2/caketron2.X/main.c
@@ -2,14 +2,42 @@
 #include "types.h"
 #include <p32xxxx.h>
 
+/* Enable secondary oscillator */
+#pragma config FSOSCEN = ON
+
 int main(void)
 {
-    DEVCFG1bits.FNOSC = 011;
-    DEVCFG1bits.POSCMOD = 10;
-    DEVCFG2bits.FPLLIDIV = 2;
-    DEVCFG2bits.FPLLMUL = 18;
-    OSCCONbits.PLLODIV = 1;
+    /* Warm-up period */
+    while (!OSCCONbits.SOSCRDY);
 
+    /* Initialize LED */
+    LATFbits.LATF1 = 0;
+    TRISFbits.TRISF1 = 0;
+
+    /* Initialize Button */
+    TRISDbits.TRISD7 = 1;
+
+    /* Initialize Timer2*/
+    T2CONbits.ON = 0;
+    T1CONbits.TCS = 1;
+    T1CONbits.TSYNC = 1;
+    T2CONbits.TCKPS = 3;
+    TMR2 = 0;
+    PR2 = 128;
+    IPC2bits.IC2IP = 1;
+    IPC2bits.IC2IS = 1;
+    IFS0bits.T2IF = 0;
+    T2CONbits.ON = 1;
+
+    while (1)
+    {
+        while (!IFS0bits.T2IF);
+        LATFbits.LATF1 = 1;
+        IFS0bits.T2IF = 0;
+        while (!IFS0bits.T2IF);
+        LATFbits.LATF1 = 0;
+        IFS0bits.T2IF = 0;
+    }
     return (0);
 }
 
