e652a41acd256505a090882305a86b8de1c54691
d4ryl00 <rbarbero@student.42.fr>
2018-04-05 11:57:12 +0200

    breath

--- caketron2/caketron2.X/main.c
+++ caketron2/caketron2.X/main.c
@@ -5,8 +5,9 @@
 #include <sys/attribs.h>
 //#pragma config FSOSCEN = ON
 
-u8 divider = 1;
-u8  dimp = 25;
+u8  divider = 1;
+u8  dimp = 1;
+s8  dim_dir = 1;
 
 /* Enable secondary oscillator */
 void	__ISR	(_TIMER_2_VECTOR, IPL7) Int_LED(void)
@@ -23,12 +24,20 @@ void	__ISR	(_EXTERNAL_1_VECTOR, IPL7) Int_BUT(void)
     IFS0bits.INT1IF = 0;
 }
 
-void	__ISR	(_TIMER_3_VECTOR, IPL7) Int_LED(void)
+void	__ISR	(_TIMER_3_VECTOR, IPL7) Int_LED_DIM(void)
 {
     IFS0bits.T3IF = 0;
-    while (TMR3 < dimp * PR3 * 0,01)
+    while (TMR3 <= dimp * PR3 / 100)
         LATFbits.LATF1 = 1;
     LATFbits.LATF1 = 0;
+}
+
+void	__ISR	(_TIMER_4_VECTOR, IPL7) Int_LED_DIM_CHANGE(void)
+{
+    IFS0bits.T4IF = 0;
+    if (dimp == 99 || dimp == 0)
+        dim_dir *= -1;
+    dimp += dim_dir;
 }
 
 int main(void)
@@ -43,11 +52,11 @@ int main(void)
     /* Initialize Button */
     TRISDbits.TRISD8 = 1;
 
-    /* Initialize Timer2 */
+    /* Initialize Timer2 = 1s */
     T2CONbits.ON = 0;
     TMR2 = 0;
-    T2CONbits.TCKPS = 4;
-    PR2 = 125000;
+    T2CONbits.TCKPS = 0b100;
+    PR2 = 62500;
 
     /* TIMER2 Interrupt configuration */
     IEC0bits.T2IE = 0;
@@ -59,8 +68,8 @@ int main(void)
     /* Initialize Timer3 */
     T3CONbits.ON = 0;
     TMR3 = 0;
-    T3CONbits.TCKPS = 4;
-    PR3 = 2500;
+    T3CONbits.TCKPS = 0b100;
+    PR3 = 625;
 
     /* TIMER3 Interrupt configuration */
     IEC0bits.T3IE = 0;
@@ -68,6 +77,19 @@ int main(void)
     IPC3bits.T3IP = 7;
     IPC3bits.T3IS = 0;
     IEC0bits.T3IE = 1;
+
+    /* Initialize Timer4 */
+    T4CONbits.ON = 0;
+    TMR4 = 0;
+    T4CONbits.TCKPS = 0b100; // = /16
+    PR4 = 3125; // 625 * 5
+
+    /* TIMER4 Interrupt configuration */
+    IEC0bits.T4IE = 0;
+    IFS0bits.T4IF = 0;
+    IPC4bits.T4IP = 7;
+    IPC4bits.T4IS = 0;
+    IEC0bits.T4IE = 1;
 
     /* Button Interrupt config*/
     IEC0bits.INT1IE = 0;
@@ -81,6 +103,8 @@ int main(void)
     __builtin_enable_interrupts();
    /* Enable Timer2 */
     T2CONbits.ON = 1;
+    T3CONbits.ON = 1;
+    T4CONbits.ON = 1;
 
     while (1)
     {
