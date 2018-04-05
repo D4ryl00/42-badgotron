9e501f1603cf84a1c39d22b98a279354e46403b6
d4ryl00 <rbarbero@student.42.fr>
2018-04-05 10:52:07 +0200

    dim

--- caketron2/caketron2.X/main.c
+++ caketron2/caketron2.X/main.c
@@ -5,7 +5,8 @@
 #include <sys/attribs.h>
 //#pragma config FSOSCEN = ON
 
-int divider = 1;
+u8 divider = 1;
+u8  dimp = 25;
 
 /* Enable secondary oscillator */
 void	__ISR	(_TIMER_2_VECTOR, IPL7) Int_LED(void)
@@ -20,6 +21,14 @@ void	__ISR	(_EXTERNAL_1_VECTOR, IPL7) Int_BUT(void)
     divider = divider == 32 ? 1 : divider * 2;
     PR2 = 125000 / divider;
     IFS0bits.INT1IF = 0;
+}
+
+void	__ISR	(_TIMER_3_VECTOR, IPL7) Int_LED(void)
+{
+    IFS0bits.T3IF = 0;
+    while (TMR3 < dimp * PR3 * 0,01)
+        LATFbits.LATF1 = 1;
+    LATFbits.LATF1 = 0;
 }
 
 int main(void)
@@ -45,7 +54,20 @@ int main(void)
     IFS0bits.T2IF = 0;
     IPC2bits.T2IP = 7;
     IPC2bits.T2IS = 0;
-    IEC0bits.T2IE = 1;
+    //IEC0bits.T2IE = 1;
+
+    /* Initialize Timer3 */
+    T3CONbits.ON = 0;
+    TMR3 = 0;
+    T3CONbits.TCKPS = 4;
+    PR3 = 2500;
+
+    /* TIMER3 Interrupt configuration */
+    IEC0bits.T3IE = 0;
+    IFS0bits.T3IF = 0;
+    IPC3bits.T3IP = 7;
+    IPC3bits.T3IS = 0;
+    IEC0bits.T3IE = 1;
 
     /* Button Interrupt config*/
     IEC0bits.INT1IE = 0;
